acf63a1fe3f703010fa368521bcfc5cdb765f2ee
João Victor Batista <jvtexbat@hotmail.com>
2022-12-04 23:49:54 -0300

    update map and tiles

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,15 +2,42 @@ import React, { useState } from "react";
 import { Tile } from "./Tile";
 
 export function Map() {
-    const [selectEnabled, setSelectEnabled] = useState(false);
+    const [interactEnabled, setInteractEnabled] = useState(false);
     const [walkableTiles, setWalkableTiles] = useState();
 
-    const tiles = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    const tiles = []
+    for (var i = 1; i <= 100; i++) {
+        tiles.push(
+            {
+                position: i,
+                type: 'floor'
+            }
+        );
+    }
+
+    const changeTileType = (position) => {
+        const currentType = tiles.find(tile => tile.position == position).type;
+        let newType = "";
+
+        if(currentType == 'tree') newType = 'floor';
+        else if (currentType == 'floor') newType = 'sticks';
+        else newType = 'tree';
+
+        tiles.find(tile => tile.position == position).type = newType;
+        console.log(tiles.find(tile => tile.position == position).type);
+    }
 
     return (
         <div className="bg-slate-600 grid gap-1 grid-cols-10 grid-rows-10">
             {
-                tiles.map((tile, index) => (<Tile key={index} name={tile} />))
+                tiles.map((tile, index) => (
+                    <Tile
+                        key={index}
+                        tile={tile}
+                        isInteractive={interactEnabled}
+                        changeType={changeTileType}
+                    />
+                ))
             }
         </div>
     )
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,37 +1,29 @@
 import React, { useState } from "react";
 
-export function Tile({ isTouchable }) {
-    const tileVariants = [
-        {
-            type: 'floor',
-            img: ''
+export function Tile({ tile, isInteractive, changeType }) {
+    const tileVariants = {
+        "floor": {
+            img: 'floor'
         },
-        {
-            type: 'sticks',
-            img: ''
+        "sticks": {
+            img: 'sticks'
         },
-        {
-            type: 'tree',
-            img: ''
-        }
-    ]
-
-    const [mode, setMode] = useState(tileVariants[0]);
-    const [touchable, setTouchable] = useState(isTouchable);
-
-    const changeMode = () => {
-        if(mode.type == 'tree') setMode(tileVariants[0]);
-        else if (mode.type == 'floor') setMode(tileVariants[1])
-        else setMode(tileVariants[2])
+        "tree": {
+            img: 'tree'
+        },
     }
+    const [mode, setMode] = useState(tile.type);
 
     return (
         <button
-            onClick={changeMode}
-            disabled={touchable}
+            onClick={() => {
+                changeType(tile.position);
+                setMode(tile.type);
+            }}
+            disabled={!isInteractive}
             className="bg-slate-400 h-10 w-10 flex items-center justify-center"
         >
-            {mode.type}
+            {tileVariants[mode].img}
         </button>
     )
 }
